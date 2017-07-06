57afab187e527fcc5b15abeff0e89f91d97fa9f3
Slaheddine DALDOUL <daldoul.slaheddine@gmail.com>
2017-07-06 16:42:51 +0200

    First commit

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,58 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+        $scope.event = {
+
+            name : 'Angular js',
+            date : '1/1/2013',
+            time : '10:30 am',
+            location : {
+
+                adress : 'google paris',
+                city : 'Nanterre',
+                province : 'PR'
+            },
+            imageUrl : '/img/angularjs-logo.png',
+            sessions : [
+
+                {
+                    name : 'Directive',
+                    creatorName : 'daldoul',
+                    duration : 'ada',
+                    level: 'adazd',
+                    abstract : 'azdaz',
+                    upVoteCount :0
+    },
+                {
+                    name : 'scopa',
+                    creatorName : 'daldoul',
+                    duration : 'ada',
+                    level: 'adazd',
+                    abstract : 'azdaz',
+                    upVoteCount :0
+                },
+                {
+                    name : 'Directive',
+                    creatorName : 'daldoul',
+                    duration : 'ada',
+                    level: 'adazd',
+                    abstract : 'azdaz',
+                    upVoteCount :0
+                }
+            ]
+        }
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;
+        }
+
+        $scope.scrollToSession = function() {
+            $anchorScroll();
+        }
+
+    }
+);
