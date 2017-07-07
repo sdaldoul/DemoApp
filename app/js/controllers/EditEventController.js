7312ce5a48fe362eea6fad7f384de9a7fda95911
Slaheddine DALDOUL <daldoul.slaheddine@gmail.com>
2017-07-07 19:49:27 +0200

    validation play

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,7 +2,15 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
-        
+
+
+
+        $scope.saveEvent = function(event, newEventForm) {
+            console.log(newEventForm);
+            if(newEventForm.$valid) {
+                window.alert('event ' + event.name + ' saved')
+            }
+        };
 
         $scope.cancelEvent = function() {
             window.location = '/EventDetails.html';
