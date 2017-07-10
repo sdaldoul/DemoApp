bfd8793c9505bbf9d9f216743e611fc4eb240e89
Slaheddine DALDOUL <daldoul.slaheddine@gmail.com>
2017-07-10 14:27:37 +0200

    http service wawwwwoo

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,14 +1,19 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope , eventData) {
+    function EventController($scope , eventData , $log) {
         $scope.snippet = '<span style ="color:red"> hi there <span/>';
         $scope.boolValue = true;
         $scope.sortorder = 'name';
         $scope.buttonDisabled = true;
         $scope.mystyle = {color:'red'};
         $scope.myclass = "blue";
-        $scope.event = eventData.event;
+
+        eventData.getEvent().success(function(event) {$scope.event = event;})
+            .error(function (data,status,header,config) {
+            $log.warn(data,status,header,config);
+        });
+
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
         };
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,48 +1,9 @@
-eventsApp.factory('eventData', function() {
+eventsApp.factory('eventData', function($http) {
     return {
 
-        event :{
-
-
-
-                name : 'Angular js 3',
-                    date : '1/1/2013',
-                time : '10:30 am',
-                location : {
-
-                adress : 'google paris',
-                    city : 'Nanterre',
-                    province : 'PR'
-            },
-                imageUrl : '/img/angularjs-logo.png',
-                    sessions : [
-
-                {
-                    name : 'Directive',
-                    creatorName : 'daldoul',
-                    duration : 1,
-                    level: 'advanced',
-                    abstract : 'azdaz',
-                    upVoteCount :0
-                },
-                {
-                    name : 'scopa',
-                    creatorName : 'daldoul',
-                    duration : 2,
-                    level: 'adazd',
-                    abstract : 'azdaz',
-                    upVoteCount :0
-                },
-                {
-                    name : 'Directive',
-                    creatorName : 'daldoul',
-                    duration : 4,
-                    level: 'adazd',
-                    abstract : 'azdaz',
-                    upVoteCount :0
-                }
-            ]
-    }
+        getEvent : function () {
+           return $http({methode : 'GET', url: 'data/event/1'});
+        }
 
     };
 });
