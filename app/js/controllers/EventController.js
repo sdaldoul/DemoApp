98533fc7c80deeefcc1ee5b157331c38ce1d256d
Slaheddine DALDOUL <daldoul.slaheddine@gmail.com>
2017-07-10 11:57:03 +0200

    buildGravatarUrl service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,53 +1,14 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope) {
+    function EventController($scope , eventData) {
         $scope.snippet = '<span style ="color:red"> hi there <span/>';
         $scope.boolValue = true;
         $scope.sortorder = 'name';
         $scope.buttonDisabled = true;
         $scope.mystyle = {color:'red'};
         $scope.myclass = "blue";
-        $scope.event = {
-
-            name : 'Angular js 3',
-            date : '1/1/2013',
-            time : '10:30 am',
-            location : {
-
-                adress : 'google paris',
-                city : 'Nanterre',
-                province : 'PR'
-            },
-            imageUrl : '/img/angularjs-logo.png',
-            sessions : [
-
-                {
-                    name : 'Directive',
-                    creatorName : 'daldoul',
-                    duration : 1,
-                    level: 'advanced',
-                    abstract : 'azdaz',
-                    upVoteCount :0
-    },
-                {
-                    name : 'scopa',
-                    creatorName : 'daldoul',
-                    duration : 2,
-                    level: 'adazd',
-                    abstract : 'azdaz',
-                    upVoteCount :0
-                },
-                {
-                    name : 'Directive',
-                    creatorName : 'daldoul',
-                    duration : 4,
-                    level: 'adazd',
-                    abstract : 'azdaz',
-                    upVoteCount :0
-                }
-            ]
-        }
+        $scope.event = eventData.event;
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
         };
